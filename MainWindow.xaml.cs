0a7733815015ebc9acf6242816c738f12c230398
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2024-03-16 21:34:43 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EFCoreDB_HW;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private readonly DataContext _dataContext;
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("config.json")
+            .Build();
+
+        DbContextOptions<DataContext> options = new DbContextOptionsBuilder<DataContext>()
+            .UseSqlServer(config.GetConnectionString("Default"))
+            .Options;
+        _dataContext = new DataContext(options);
+    }
+}
